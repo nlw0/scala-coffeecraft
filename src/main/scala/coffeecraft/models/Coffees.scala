3fad74bd61e054d2f4675093d09db7aee615598c
nlw0 <nwerneck@gmail.com>
2015-06-20 01:25:57 -0300

    Starting to figure out a fun "crafting" kind of thing

--- src/main/scala/coffeecraft/models/Coffees.scala
+++ src/main/scala/coffeecraft/models/Coffees.scala
@@ -5,6 +5,17 @@ import slick.driver.H2Driver.api._
 
 case class Coffee(name: String, price: Double, id: Option[CoffeeKey] = None) extends EntityWithId {
   def updateId(newId: CoffeeKey) = Coffee(name, price, id = Some(newId))
+
+  def craft(that: Coffee) = {
+
+    val recipes = Map(
+      List("Jameson", "Milk") -> "Irish",
+      List("Chocolate", "Milk") -> "Cappuccino"
+    )
+
+    val result = recipes(List(this.name, that.name) sorted)
+    Coffee(result, this.price + that.price)
+  }
 }
 
 
