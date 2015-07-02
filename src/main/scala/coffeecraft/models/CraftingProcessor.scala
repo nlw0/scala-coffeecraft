3e76bdf6622dbe766adea33233d03bd7b7c25cc8
nlw0 <nwerneck@gmail.com>
2015-07-02 00:56:23 -0300

    Tweaking "coffee" probability.

--- src/main/scala/coffeecraft/models/CraftingProcessor.scala
+++ src/main/scala/coffeecraft/models/CraftingProcessor.scala
@@ -15,9 +15,14 @@ class BaseCraftingProcessor(val craftingFunction: Map[CoffeeIdSet, Coffee]) {
   // TODO: retrieve mining probabilities from some table
   def mine(forFree: Boolean = false) = {
     val idLimit = if (forFree) 3 else 6
-    if (scala.util.Random.nextInt(2) == 0)
-      Await.result(CoffeeDao.fetchOneById(scala.util.Random.nextInt(idLimit).toLong + 1L), Duration.Inf)
-    else None
+    scala.util.Random.nextInt(3) match {
+      case 0 => None
+      case 1 =>
+        Await.result(CoffeeDao.fetchOneById(1L), Duration.Inf)
+      case 2 =>
+        val newId = scala.util.Random.nextInt(idLimit).toLong + 1L
+        Await.result(CoffeeDao.fetchOneById(newId), Duration.Inf)
+    }
   }
 }
 
